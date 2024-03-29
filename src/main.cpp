9fa2f06992a2eb41098329cefa58810ffcd5cb9c
oddvare <oddvar.engedal@gmail.com>
2024-03-29 12:26:06 +0100

    continue working

--- src/main.cpp
+++ src/main.cpp
@@ -24,13 +24,17 @@ int servoPin = 14;
 //=======GPS=========
 #define GPS_RX 16  // GPS module RX pin
 #define GPS_TX 17  // GPS module TX pin
+#define firsttime 0
 
 HardwareSerial gpsSerial(1);
 TinyGPSPlus gps;
 
 //======our target
-const float pos_lat =59.3528644;
-const float pos_long =5.2192641;
+//const float pos_lat =59.3528644;
+//const float pos_long =5.2192641;
+//=====test target  
+const float pos_lat = 59.400607;
+const float pos_long = 5.2833096;
 
 // put function declarations here:
 int myFunction(int, int);
@@ -44,15 +48,96 @@ int myFunction(int, int);
 //if the user has reached the target, the lights will turn green and the servo will turn to 10 degrees
 //picture of suspicius person carrying a box. The user will be able to click on the picture to get a hint(maybe)
 const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html><head>
+  <title>Fritz portal</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head><body>
+  <h3>Fritz box</h3>
+  <br><br>
+  <h4>Distance to target:</h4>
+  <table> 
+  <tr>
+  <th>Distance (km)</th>  
+  <th>Course to target</th> 
+  </tr>  
+  <tr>  
+  <td>distanceKm</td> 
+  <td>courseTo</td>
+  </tr>
+  </table>
+  <br><br>
+  
+  <pre>
+  <h4>Hint:</h4>
+  <ul>  
+  <li>Cu</li>
+  <li>NY</li>
+  <li>Kolhusvegen 116</li>
+  </ul>
+
+
+  </pre>  
+  <button onclick="window.location.href='/hint'">Check location</button>
+  <button onclick="window.location.href='/hint'">Get hint</button>
+  </body></html>
+  )rawliteral";
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  
+  //your other setup stuff...
+  //serial for debugging
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Setting up AP Mode");
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP("Mystery-box");
+  Serial.print("AP IP address: ");Serial.println(WiFi.softAPIP());
+  Serial.println("Setting up Async WebServer");
+  setupServer();
+  Serial.println("Starting DNS Server");
+  dnsServer.start(53, "*", WiFi.softAPIP());
+  server.addHandler(new CaptiveRequestHandler()).setFilter(ON_AP_FILTER);//only when requested from AP
+  //more handlers...
+  //Setup for servo
+  myservo.setPeriodHertz(50);
+  myservo.attach(servoPin);
+
+  server.begin();
+  Serial.println("All Done!");
+  Serial.println("Rødt lys");
+  FastLED.addLeds<NEOPIXEL, dataPin>(leds, numLeds);
+  //for (p=0; p <= 4; p += 1) {
+  //leds[p]= CRGB::Red;
+  //tester åpen posisjon
+  fill_solid(leds,5,CRGB::GreenYellow);
+  myservo.write(179);
+  FastLED.show();
+  //tester lukket
+  delay(2000);
+  myservo.write(180);
+  fill_solid(leds,5,CRGB::CornflowerBlue);
+  FastLED.show();
+  delay(2000);
+  //lar den stå i åpen posisjon
+  myservo.write(10);
+  //}
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  double distanceKm =
+  //set a timer to show the user a new hint every 10 minutes
+  //set a timer to show the user a new hint every 5 minutes after the first 10 minutes
+  bool isFirstTime = true; // Add this line
+    dnsServer.processNextRequest();
+    
+    if (isFirstTime) { // Add this if condition
+      firsttime = timeClient.now();
+      isFirstTime = false;
+    }
+    
+    // Rest of the code...
+
+ 
+double distanceKm =
   gps.distanceBetween(
     gps.location.lat(),
     gps.location.lng(),
@@ -74,7 +159,7 @@ Serial.println(gps.cardinal(courseTo));
 //if the user has reached the destination, the lights will turn green
 //and the servo will turn to 10 degrees
 
-if (distanceKm < 0.1) {
+if (distanceKm < 0.02) {
   Serial.println("You have reached your destination!");
 
   delay(1000);
